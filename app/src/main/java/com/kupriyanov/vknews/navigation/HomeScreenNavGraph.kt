8da3acd81afbe767e31e54970a626ddc9d4a9446
dmitrykupriyanov <Dmitry.Kupriyanov@lppsa.com>
2023-06-13 15:06:19 +0300

    передача целого объекта в навигации

--- app/src/main/java/com/kupriyanov/vknews/navigation/HomeScreenNavGraph.kt
+++ app/src/main/java/com/kupriyanov/vknews/navigation/HomeScreenNavGraph.kt
@@ -2,8 +2,11 @@ package com.kupriyanov.vknews.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navigation
+import com.google.gson.Gson
 import com.kupriyanov.vknews.domain.FeedPost
 
 fun NavGraphBuilder.homeScreenNavGraph(
@@ -17,9 +20,17 @@ fun NavGraphBuilder.homeScreenNavGraph(
         composable(Screen.NewsFeed.route) {
             newsFeedScreenContent()
         }
-        composable(Screen.Comments.route) {
-            val feedPostId = it.arguments?.getInt(Screen.KEY_FEED_POST_ID) ?: 0
-            commentsScreenContent(FeedPost(id = feedPostId))
+        composable(
+            route = Screen.Comments.route,
+            arguments = listOf(
+                navArgument(Screen.KEY_FEED_POST) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val feedPostJson = it.arguments?.getString(Screen.KEY_FEED_POST) ?: ""
+            val feedPost = Gson().fromJson(feedPostJson, FeedPost::class.java)
+            commentsScreenContent(feedPost)
         }
     }
 }
--- app/src/main/java/com/kupriyanov/vknews/navigation/Screen.kt
+++ app/src/main/java/com/kupriyanov/vknews/navigation/Screen.kt
@@ -1,5 +1,7 @@
 package com.kupriyanov.vknews.navigation
 
+import android.net.Uri
+import com.google.gson.Gson
 import com.kupriyanov.vknews.domain.FeedPost
 
 sealed class Screen(
@@ -14,18 +16,23 @@ sealed class Screen(
 
         private const val ROUTE_FOR_ARGS = "comments"
         fun getRouteWithArgs(feedPost: FeedPost): String {
-            return "$ROUTE_FOR_ARGS/${feedPost.id}"
+            val feedPostJson = Gson().toJson(feedPost)
+            return "$ROUTE_FOR_ARGS/${feedPostJson.encode()}"
         }
     }
 
     companion object {
 
-        const val KEY_FEED_POST_ID = "feed_post_id"
+        const val KEY_FEED_POST = "feed_post"
 
         const val ROUTE_HOME = "home"
-        const val ROUTE_COMMENTS = "comments/{KEY_FEED_POST_ID}"
+        const val ROUTE_COMMENTS = "comments/{$KEY_FEED_POST}"
         const val ROUTE_NEWS_FEED = "news_feed"
         const val ROUTE_FAVOURITE = "favourite"
         const val ROUTE_PROFILE = "profile"
     }
+}
+
+fun String.encode(): String {
+    return Uri.encode(this)
 }
